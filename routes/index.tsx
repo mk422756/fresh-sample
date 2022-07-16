aa926f52a4c393dacd078ac2174f199871819914
mk422756 <k422756+git@gmail.com>
2022-07-16 17:19:53 +0900

    import test

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,8 +2,10 @@
 import { h } from "preact";
 import { tw } from "@twind";
 import Counter from "../islands/Counter.tsx";
+import { test } from "@/server/test.ts"
 
 export default function Home() {
+  const t = test()
   return (
     <div class={tw`p-4 mx-auto max-w-screen-md`}>
       <img
@@ -13,7 +15,7 @@ export default function Home() {
       />
       <p class={tw`my-6`}>
         Welcome to `fresh`. Try update this message in the ./routes/index.tsx
-        file, and refresh.
+        file, and refresh. {t}
       </p>
       <Counter start={3} />
     </div>
